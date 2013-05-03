bccf1e3cc3cba19bfce48ed3aebe36af61b68b2b
Raghu Betina <rbetina@gmail.com>
2013-05-03 11:36:42 -0500

    Added has_many through association method to actor model.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,17 @@
 class Actor < ActiveRecord::Base
   has_many :roles
+  has_many :movies, :through => :roles
 
   validates :name, presence: true
+
+  # def movies
+  #   movies_array = []
+  #   roles_array = self.roles
+  #   roles_array.each do |role|
+  #     movies_array << role.movie
+  #   end
+  #   return movies_array
+  # end
 
   # def roles
   #   return Role.where(:actor_id => self.id)
