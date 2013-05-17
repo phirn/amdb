4bca7cc395a95b68bf647bd7fbea69d9a2c3351b
Raghu Betina <rbetina@gmail.com>
2013-05-17 08:58:26 -0500

    Fixed a bug introduced last class with sign-in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,16 @@
 class SessionsController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    u = User.find_by_username(params[:username])
+    @user = User.find_by_username(params[:username])
 
-    if u.present? && u.authenticate(params[:password])
-      session[:user_id] = u.id
+    if @user.present? && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
       redirect_to movies_url, notice: 'Signed in successfully.'
     else
-      redirect_to new_session_url, notice: 'Nice try.'
+      render 'new'
     end
   end
 
