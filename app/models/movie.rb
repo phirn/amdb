85295b37d2031063fa7ab5427ca07fe6966963ce
Raghu Betina <rbetina@gmail.com>
2013-06-03 10:14:46 -0500

    Added .try examples. Added destroy dependency to a movie's roles.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,11 @@ class Movie < ActiveRecord::Base
   attr_accessible :title, :year, :director_id
 
   belongs_to :director
-  has_many :roles
+  has_many :roles, :dependent => :destroy
   has_many :actors, :through => :roles
   has_many :votes
 
   validates :title, presence: true
-  validates :director, presence: true
   validates :year, presence: true
 
   # def director
